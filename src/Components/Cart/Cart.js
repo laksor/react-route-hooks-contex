903c151e383f515375f913e3dd030ca5ac098093
laskor <al.moksud@gmail.com>
2022-04-09 02:58:31 -0400

    handle

--- src/Components/Cart/Cart.js
+++ src/Components/Cart/Cart.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import './Cart.css';
 
-const Cart = () => {
+const Cart = ({cart, handleRemove}) => {
     return (
         <div>
             <h1>This is Cart</h1>
+            {
+                cart.map(tShirt => <p> 
+                    {tShirt.name} 
+                    <button onClick={() => handleRemove(tShirt)}>x</button>
+                    </p> )
+            }
         </div>
     );
 };
--- src/Components/TShirt/TShirt.js
+++ src/Components/TShirt/TShirt.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import './TShirt.css';
 
-const TShirt = (props) => {
-    const {name, picture, price} = props.tShirt;
+const TShirt = ({handleAddToCart, tShirt}) => {
+    const {name, picture, price} = tShirt;
     return (
         <div className="TShirt-t">
             <img src={picture} alt="" />
             <h1>{name}</h1>
-            <p>Price: ${price}</p>
-            <button >Add to cart</button>
+            <p>Price: $ {price}</p>
+            <button onClick={() => handleAddToCart(tShirt)}>Add to cart</button>
         </div>
     );
 };
