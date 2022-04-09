7635529e8d1aeb51ef29e7cd2a002d776cb4624a
laskor <al.moksud@gmail.com>
2022-04-09 01:57:59 -0400

    components added

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import Header from './Components/Header/Header';
+import Home from './Components/Home/Home';
 
 function App() {
   return (
     <div className="App">
-      <h1>Header</h1>
+      <Header></Header>
+      <Home></Home>
     </div>
   );
 }
