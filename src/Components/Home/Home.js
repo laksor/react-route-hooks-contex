1d2191db2eec7df9e5e7bc8386dcea0fff26795f
laskor <al.moksud@gmail.com>
2022-04-10 00:31:37 -0400

    context api

--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -1,49 +1,48 @@
-import React, { useState } from 'react';
-import Cart from '../Cart/Cart';
-import useTShirt from '../hooks/useTShirt';
-import TShirt from '../TShirt/TShirt';
-import './Home.css';
+import React, { createContext, useState } from "react";
+import Cart from "../Cart/Cart";
+import useTShirt from "../hooks/useTShirt";
+import TShirt from "../TShirt/TShirt";
+import "./Home.css";
+
+export  const RingContext = createContext('Ring');
 
 const Home = () => {
-    const [tShirts, setTShirts] = useTShirt();
-    const [cart, setCart] = useState([]);
+  const [tShirts, setTShirts] = useTShirt();
+  const [cart, setCart] = useState([]);
 
-    const handleAddToCart = (selectedItem) => {
-        const exist = cart.find(tShirt => tShirt._id === selectedItem._id);
-        if(!exist){
-            const newCart = [...cart, selectedItem];
-            setCart(newCart);
-        }
-        else{
-            alert('item already added');
-        }
-        
+  const handleAddToCart = (selectedItem) => {
+    const exist = cart.find((tShirt) => tShirt._id === selectedItem._id);
+    if (!exist) {
+      const newCart = [...cart, selectedItem];
+      setCart(newCart);
+    } else {
+      alert("item already added");
     }
+  };
 
-    const handleRemove = (selectedItem) =>{
-        const rest = cart.filter(tShirt => tShirt._id !== selectedItem._id);
-        setCart(rest);
-    }
-    return (
-        <div className="home-container">
-            <div className="tShirt-container">
-                {
-                    tShirts.map(tShirt => <TShirt
-                        key={tShirt._id}
-                        tShirt={tShirt}
-                        handleAddToCart={handleAddToCart}
-                        ></TShirt>)
-                }
-            </div>
-
-            <div className="cart-container">
-                <Cart 
-                cart={cart}
-                handleRemove={handleRemove}
-                ></Cart>
-            </div>
+  const handleRemove = (selectedItem) => {
+    const rest = cart.filter((tShirt) => tShirt._id !== selectedItem._id);
+    setCart(rest);
+  };
+  return (
+    <RingContext.Provider value='alur ring'>
+      <div className="home-container">
+        <div className="tShirt-container">
+          {tShirts.map((tShirt) => (
+            <TShirt
+              key={tShirt._id}
+              tShirt={tShirt}
+              handleAddToCart={handleAddToCart}
+            ></TShirt>
+          ))}
         </div>
-    );
+
+        <div className="cart-container">
+          <Cart cart={cart} handleRemove={handleRemove}></Cart>
+        </div>
+      </div>
+    </RingContext.Provider>
+  );
 };
 
 export default Home;
--- src/Components/OrderReview/OrderReview.js
+++ src/Components/OrderReview/OrderReview.js
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { RingContext } from '../Home/Home';
 
 const OrderReview = () => {
+    const ring = useContext(RingContext);
     return (
         <div>
             <h1>Order review</h1>
+            <p>context : {ring}</p>
         </div>
     );
 };
